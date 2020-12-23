09035d929292efb5aa1d56eca49b506fa7355d35
David Ong <dodwei.dev@gmail.com>
2020-12-23 11:21:05 +0800

    Finish day16 Part2

--- day16/main.go
+++ day16/main.go
@@ -1,197 +1,179 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
+	"gnodivad/advent-of-code/utils"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-type Boundary struct {
-	low  int
-	high int
+type Puzzle struct {
+	rules        []*Rule // Store the address of rule object to make update pos easier in Part 2
+	myTicket     Ticket
+	otherTickets []Ticket
 }
+
+type Ticket []int
 
 type Rule struct {
 	name       string
-	boundaries []Boundary
+	pos        int
+	boundaries [2][2]int
+}
+
+func main() {
+	puzzle := createPuzzle(utils.ReadStringsFromFile("day16/input.txt"))
+
+	fmt.Println("--- Part One ---")
+	fmt.Println(puzzle.calculateErrorRate())
+
+	fmt.Println("--- Part Two ---")
+	fmt.Println(puzzle.getMultiplicationOfAllDepartureField())
+}
+
+func createPuzzle(notes []string) Puzzle {
+	p := Puzzle{}
+
+	zone := 0
+
+	for _, note := range notes {
+		if note == "" {
+			zone++
+			continue
+		}
+
+		if strings.Contains(note, "your ticket:") || strings.Contains(note, "nearby tickets:") {
+			continue
+		}
+
+		if zone == 0 {
+			p.addNewRule(note)
+		} else if zone == 1 {
+			p.myTicket = createNewTicket(note)
+		} else if zone == 2 {
+			p.otherTickets = append(p.otherTickets, createNewTicket(note))
+		}
+	}
+
+	return p
 }
 
 var regexForField = regexp.MustCompile(`(\D+):\s(\d+)-(\d+)\sor\s(\d+)-(\d+)`)
 
-func main() {
-	notes := readStringsFromFile("day16/input.txt")
-
-	rules := make([]Rule, 0)
-	myTicket := make([]int, 0)
-
-	isField, isTicket, isNearbyTicket := true, false, false
-	possibleRules := make(map[string][]bool)
-
-	sum := 0
-	for _, note := range notes {
-		if note == "your ticket:" {
-			isField = false
-			isTicket = true
-		}
-
-		if note == "nearby tickets:" {
-			isTicket = false
-			isNearbyTicket = true
-		}
-
-		if note == "" || note == "your ticket:" || note == "nearby tickets:" {
-			continue
-		}
-
-		if isField {
-			rule := createNewRule(note)
-			rules = append(rules, rule)
-			possibleRules[rule.name] = []bool{}
-		}
-
-		if isTicket {
-			for _, ticketAsString := range strings.Split(note, ",") {
-				ticketAsInt, _ := strconv.Atoi(ticketAsString)
-				myTicket = append(myTicket, ticketAsInt)
-			}
-
-			for name := range possibleRules {
-				possibleRules[name] = make([]bool, len(myTicket))
-				for i := range possibleRules[name] {
-					possibleRules[name][i] = true
-				}
-			}
-		}
-
-		if isNearbyTicket {
-			tickets := make([]int, 0)
-			var notPossibleRulesline [][]string
-			var valid bool
-			for _, ticketAsString := range strings.Split(note, ",") {
-				ticketAsInt, _ := strconv.Atoi(ticketAsString)
-				tickets = append(tickets, ticketAsInt)
-
-				valid = false
-				var notPossibleRules []string
-				for _, rule := range rules {
-					if rule.isFullfilledBy(ticketAsInt) {
-						valid = true
-					} else {
-						notPossibleRules = append(notPossibleRules, rule.name)
-					}
-				}
-				notPossibleRulesline = append(notPossibleRulesline, notPossibleRules)
-			}
-
-			if valid {
-				for i, notPossibleRules := range notPossibleRulesline {
-					for _, name := range notPossibleRules {
-						possibleRules[name][i] = false
-					}
-				}
-			}
-
-			sum += getInvalidField(tickets, rules)
-		}
-	}
-
-	fmt.Println("--- Part One ---")
-	fmt.Println(sum)
-
-	fmt.Println("--- Part Two ---")
-
-	fmt.Println(possibleRules)
-}
-
-func createNewRule(note string) Rule {
+func (p *Puzzle) addNewRule(note string) {
 	r := regexForField.FindStringSubmatch(note)
 	g1Lower, _ := strconv.Atoi(r[2])
 	g1Upper, _ := strconv.Atoi(r[3])
 	g2Lower, _ := strconv.Atoi(r[4])
 	g2Upper, _ := strconv.Atoi(r[5])
 
-	return Rule{
+	p.rules = append(p.rules, &Rule{
 		name:       r[1],
-		boundaries: []Boundary{{low: g1Lower, high: g1Upper}, {low: g2Lower, high: g2Upper}},
-	}
+		pos:        -1,
+		boundaries: [2][2]int{{g1Lower, g1Upper}, {g2Lower, g2Upper}},
+	})
 }
 
-func assignFields(nearbyTickets [][]int, rules []Rule) {
-	allPossibleField := getAllPossibleFields(0, nearbyTickets[0], rules, make([][]string, 0), make([]string, 0), make(map[string]bool))
+func createNewTicket(note string) Ticket {
+	t := make(Ticket, 0)
 
-	fmt.Println(allPossibleField)
-}
-
-func getAllPossibleFields(index int, ticket []int, rules []Rule, allPossibleField [][]string, currentField []string, lookup map[string]bool) [][]string {
-	if index == len(ticket) {
-		allPossibleField = append(allPossibleField, currentField)
-		return allPossibleField
+	for _, v := range strings.Split(note, ",") {
+		t = append(t, utils.ParseInt(v))
 	}
 
-	for _, rule := range rules {
-		if _, ok := lookup[rule.name]; !ok {
-			if rule.isFullfilledBy(ticket[index]) {
-				lookup[rule.name] = true
-				cloneField := make([]string, len(currentField))
-				copy(cloneField, currentField)
-				cloneField = append(cloneField, rule.name)
+	return t
+}
 
-				allPossibleField = getAllPossibleFields(index+1, ticket, rules, allPossibleField, cloneField, lookup)
-			}
-			delete(lookup, rule.name)
+func (p Puzzle) calculateErrorRate() int {
+	errorRate := 0
+	for _, ticket := range p.otherTickets {
+		if isValid, invalidValue := ticket.validateByRules(p.rules); !isValid {
+			errorRate += invalidValue
 		}
 	}
 
-	return allPossibleField
+	return errorRate
 }
 
-func getInvalidField(tickets []int, rules []Rule) int {
-	for _, ticketAsInt := range tickets {
-		isValidField := false
+func (r Rule) validate(val int) bool {
+	return (val >= r.boundaries[0][0] && val <= r.boundaries[0][1]) || val >= r.boundaries[1][0] && val <= r.boundaries[1][1]
+}
 
-		for _, rule := range rules {
-			if rule.isFullfilledBy(ticketAsInt) {
-				isValidField = true
-				break
+func (ticket Ticket) validateByRules(rules []*Rule) (bool, int) {
+	for _, t := range ticket {
+		isValid := false
+
+		for _, r := range rules {
+			if r.validate(t) {
+				isValid = true
 			}
 		}
-		if !isValidField {
-			return ticketAsInt
+
+		if !isValid {
+			return false, t
 		}
 	}
 
-	return 0
+	return true, 0
 }
 
-func (rule Rule) isFullfilledBy(value int) bool {
-	for _, boundary := range rule.boundaries {
-		if boundary.validate(value) {
-			return true
+func (puzzle *Puzzle) getUnidentifiedRules() map[*Rule]bool {
+	ret := make(map[*Rule]bool)
+
+	for _, rule := range puzzle.rules {
+		if rule.pos == -1 {
+			ret[rule] = true
 		}
 	}
-	return false
+
+	return ret
 }
 
-func (boundary Boundary) validate(value int) bool {
-	return value >= boundary.low && value <= boundary.high
+func (puzzle Puzzle) identifyFields() {
+	validTickets := make([]Ticket, 0)
+	for _, ticket := range puzzle.otherTickets {
+		if isValid, _ := ticket.validateByRules(puzzle.rules); isValid {
+			validTickets = append(validTickets, ticket)
+		}
+	}
+
+	possibleRules := puzzle.getUnidentifiedRules()
+
+	for len(possibleRules) > 0 {
+		for pos := 0; pos < len(puzzle.myTicket); pos++ {
+			// For each of the rule, go through all the nearby tickets in specific position
+			for _, rule := range puzzle.rules {
+				for _, ticket := range validTickets {
+					// If the rule does not match, exclude it from possible rules list and continue to next rule
+					if !rule.validate(ticket[pos]) {
+						delete(possibleRules, rule)
+						break
+					}
+				}
+			}
+
+			if len(possibleRules) == 1 {
+				for rule := range possibleRules {
+					rule.pos = pos
+				}
+			}
+
+			possibleRules = puzzle.getUnidentifiedRules()
+		}
+	}
 }
 
-func readStringsFromFile(filepath string) (strings []string) {
-	file, err := os.Open(filepath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+func (puzzle Puzzle) getMultiplicationOfAllDepartureField() int {
+	puzzle.identifyFields()
 
-	scanner := bufio.NewScanner(file)
+	result := 1
 
-	for scanner.Scan() {
-		strings = append(strings, scanner.Text())
+	for _, rule := range puzzle.rules {
+		if strings.Contains(rule.name, "departure") {
+			result *= puzzle.myTicket[rule.pos]
+		}
 	}
 
-	return
+	return result
 }
